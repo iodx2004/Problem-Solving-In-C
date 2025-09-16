ff492dc135e6e16d5a8a06e71969d324f90c2b36
iodx2004 <iodx2004@github.com>
2025-09-16 16:25:03 +0000

    level01

--- C/Basic-Declaration-And-Expression/7th-Display-multiple-values.c
+++ C/Basic-Declaration-And-Expression/7th-Display-multiple-values.c
@@ -1,11 +1,19 @@
 #include <unistd.h>
 
+void ft_putstr(char *str)
+{
+  while(*str)
+  {
+    write(1, str++, 1);
+  }
+}
+
 void ft_putchar(char c)
 {
   write(1, &c, 1);
 }
 
-void ft_putnbr(int nb)
+void ft_putnbr(unsigned long int nb)
 {
   if(nb >= 10)
   {
@@ -22,8 +30,8 @@ void sum(int a, int b)
 
 void SumAscii(float x, char c)
 {
-  char result = x + c;
-  write(1, &result, 1);
+  int result = x + c;
+  ft_putnbr(result);
 }
 
 void SumCastingFloat(double dx, long ax)
@@ -50,4 +58,59 @@ void SumLongNumbers(long ax , int b)
   ft_putnbr(ax + b);
 }
 
+void CharWithShortNum(short int s, char c)
+{
+  char result = s + c;
+  ft_putnbr(result);
+}
 
+void CharWithLongNum(long int ax, char c)
+{
+  char result = ax + c;
+  ft_putnbr(result);
+}
+
+void LongsValueSum(long int ax, unsigned long int ux)
+{
+  unsigned long int result = ax + ux;
+  ft_putnbr(result);
+}
+
+int main()
+{
+
+  int a = 125, b = 12345;
+  long ax = 1234567890;
+  short s = 4043;
+  float x = 2.13459;
+  double dx = 1.1415927;
+  char c = 'W';
+  unsigned long ux = 2541567890;
+  
+  ft_putstr("The Result of Sum Numbers is : ");
+  sum(a, b);
+
+  ft_putstr("\nThe Result Of Sum Ascii Num is : ");
+  SumAscii(x, c);
+
+  ft_putstr("\nThe Result of sum Of Casting Floats Number is : ");
+  SumCastingFloat(dx, ax);
+
+  ft_putstr("\nThe Result Of Sum Float Numbers With Integers is : ");
+  SumFloatsWithInteger(a, x);
+
+  ft_putstr("\nThe Result Of Sum Short Numbers is : ");
+  sumSHortNumbers(s, b);
+
+  ft_putstr("\nThe Result Of Sum Long Numbers is : ");
+  SumLongNumbers(ax, b);
+
+  ft_putstr("\nThe Result Of Short Numbers With Character is : ");
+  CharWithShortNum(s, c);
+
+  ft_putstr("\nThe Result Of Long Numbers With Character is : ");
+  CharWithLongNum(ax, c);
+
+  ft_putstr("\nThe Result Of Long Number Of Values is : ");
+  LongsValueSum(ax, ux);
+}
